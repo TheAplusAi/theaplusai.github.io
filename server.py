24f8af4fecda6db7ff4ea1d647fc8191b24297fe
AplusAi <a.mishra123@hotmail.com>
2026-05-09 12:31:28 +0530

    Chat

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from openai import OpenAI
-
 # ------------------ APP SETUP ------------------
 app = Flask(__name__)
 CORS(app)
